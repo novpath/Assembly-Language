0568f5b0e1a15eefd961c674aac58f9a5a1e7ac3
novpath <563959624@qq.com>
2025-03-04 17:29:49 +0800

    :memo:实验17 编写包含多个功能子程序的中断例程

--- 源代码/ch17/lab17/lab17.asm
+++ 源代码/ch17/lab17/lab17.asm
@@ -1,5 +1,9 @@
 assume cs:code
 
+data segment
+	a db 4000 dup (0)
+data ends
+
 code segment
 start:
 	mov ax,0
@@ -19,57 +23,74 @@ start:
 	cld
 	rep movsb
 	
-	mov ax,079ah
+	mov ax,data
 	mov es,ax
-	mov al,0
-	mov ah,0
-	mov dx,1579
-	mov bx,200
+	mov bx,offset a	;es:bx 指向用于读写的缓冲区
+	mov ah,0		;ah=0 读
+	mov dx,0		;逻辑扇区号
 	int 7ch
-	nop			;调试看寄存器参数
-	mov dl,80h
-	int 13h
 	
 	mov ax,4c00h
 	int 21h
+	org 200H
 lp:
 	push bp
-	push dx
-	push cx
-	push bx
 	push ax
 	
 	cmp ah,1
 	ja ok					;功能号超出范围则退出
 	mov bp,sp
 	add ah,2
+	mov al,1				;读写扇区数 1
 	mov word ptr [bp],ax	;int 13h 功能号 2 读，3 写，而 ah 为 0 读 1 写，二者差 2
 	
+	call disknum
+	
+	int 13h					;调用 int 13h 中断例程
+ok:
+	pop ax
+	pop bp
+	iret
+;------------------------------------
+;名称：disknum
+;输入：逻辑扇区号 dx
+;返回：dh(面号)、dl(驱动器号)、ch(磁道号)、cl(扇区号)
+;------------------------------------
+disknum:
+	jmp short disknum_core
+	cst dw 1440,18
+disknum_core:
+	push bp
+	push dx
+	push cx
+	push bx
+	push ax
+
+	mov bp,sp
 	mov ax,dx
-	mov bx,1440
 	mov dx,0
-	div bx
+	div word ptr cst[0]
 	mov ah,al
 	mov al,0
-	mov word ptr [bp+6],ax	;高 8 位存面号 dh
+	mov word ptr [bp+6],ax		;高 8 位存面号 dh
+	and word ptr [bp+6],0FFF0H	;驱动器号 0,A 盘
 	
-	mov ax,dx
-	mov bx,18
+	mov ax,dx					;取余数
 	mov dx,0
-	div bx					;商 ax 为磁道号
-    mov ah,al
-    mov al,0
-    mov word ptr [bp+4],ax	;高 8 位存磁道号 ch
+	div word ptr cst[1]			;商 ax 为磁道号
+	mov ah,al
+	mov al,0
+	mov word ptr [bp+4],ax		;高 8 位存磁道号 ch
 	
-	add dx,1				;余数 dx 为扇区号
-	add word ptr [bp+4],dx	;低 8 位存扇区号 cl
-ok:
+	add dx,1					;余数 dx, +1 为扇区号
+	add word ptr [bp+4],dx		;低 8 位存扇区号 cl
+	
 	pop ax
 	pop bx
 	pop cx
 	pop dx
 	pop bp
-	iret
+	ret
 lpend:
 	nop
 	
